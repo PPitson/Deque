a43b421be1b18d16d2714e459add91571d9d7995
Your Name <czaczek19@wp.pl>
2017-05-30 10:37:21 +0200

    Add first, last and pushFront implementation

--- src/Deque.hs
+++ src/Deque.hs
@@ -30,14 +30,50 @@ popBackDEQ :: Deque a -> Maybe (a, q a)
 fromListDEQ :: [a] -> Deque a
 
 emptyDEQ = MkDeque 0 [] [] 0 [] []
+
 isEmptyDEQ (MkDeque lenf f sf lenr r sr) = (lenf + lenr) == 0
+
 lengthDEQ (MkDeque lenf f sf lenr r sr) = lenf + lenr
-firstDEQ _ = Nothing
-lastDEQ _ = Nothing
+
+firstDEQ (MkDeque lenf f sf lenr r sr) = case f of
+	[] -> case r of
+		[] -> Nothing
+		(x : tr) -> Just x
+	(x : tf) -> Just x
+
+lastDEQ (MkDeque lenf f sf lenr r sr) = case r of
+	[] -> case f of
+		[] -> Nothing
+		(x : tf) -> Just x
+	(x : tr) -> Just x
+
 takeFrontDEQ _ _ = []
+
 takeBackDEQ _ _ = []
-pushFrontDEQ _ _ = MkDeque 0 [] [] 0 [] []
+
+pushFrontDEQ (MkDeque lenf f sf lenr r sr) elem = balance (MkDeque (lenf+1) (elem:f) (drop 2 sf) lenr r (drop 2 sr))
 popFrontDEQ _ = Nothing
-pushBackDEQ _ _ = MkDeque 0 [] [] 0 [] []
+pushBackDEQ (MkDeque lenf f sf lenr r sr) elem = balance (MkDeque lenf f (drop 2 sf) (lenr+1) (elem:r) (drop 2 sr))
 popBackDEQ _ = Nothing
 fromListDEQ _ = MkDeque 0 [] [] 0 [] []
+
+
+balance :: Deque a -> Deque a
+balance (MkDeque lenf f sf lenr r sr) 
+	| lenf > 2*lenr+1 = 
+		let i = (lenf+lenr) `div` 2
+			j = lenf + lenr -i
+			f` = take i f
+			r' = rotateDrop r i f
+		in MkDeque i f' f' j r' r'
+	| lenr > 2*lenf+1 = 
+		let j = (lenf+lenr) `div` 2
+			i = lenf + lenr - j
+			r' = take i r
+			f' = rotateDrop f i r
+		in MkDeque i f' f' j r' r'
+	| otherwise = MkDeque lenf f sf lenr r sr
+
+
+rotateDrop :: [a] -> Int -> [a] -> [a]
+rotateDrop = []
