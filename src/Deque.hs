b4c8778aee925bc1a8b849a1e461c89b36e64aab
Piotr Przybyło <pitu5599@gmail.com>
2017-05-30 18:30:37 +0200

    Added Functor instance to Deque

--- src/Deque.hs
+++ src/Deque.hs
@@ -29,6 +29,9 @@ instance Show a => Show (Deque a) where
 instance Eq a => Eq (Deque a) where
   (==) (MkDeque _ f1 _ r1) (MkDeque _ f2 _ r2) = (f1 ++ r1) == (f2 ++ r2)
 
+instance Functor Deque where
+  fmap g (MkDeque lenf f lenr r) = MkDeque lenf (fmap g f) lenr (fmap g r)
+
 -- |
 -- O(1)
 emptyDEQ :: Deque a
